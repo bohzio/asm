0da6ad3d2d9df95e60fb624d459998ac81beaa81
ScodroS <40734838+ScodroS@users.noreply.github.com>
2018-07-15 15:53:44 +0200

    Add files via upload

---
+++ ASM_v3/Makefile.mk
@@ -0,0 +1,16 @@
+COMPILATORE= gcc -m32
+CODICE= controller.c genera_fascia.s
+OGGETTO= controller.o genera_fascia.o
+DEBUG= -g
+ESEGUIBILE= controller
+
+# In questa parte viene creato il file eseguibile dai file oggetto (con aggiunta di -g per poter utilizzare il debugger)
+$(ESEGUIBILE): $(OGGETTO)
+	$(COMPILATORE) $(DEBUG) $(OGGETTO) -o $(ESEGUIBILE)
+# -c è la flag da aggiungere per ottenere i file oggetto dai "file codice" con gcc, ed è uguale sia per assembly che per c
+$(OGGETTO): $(CODICE)
+	$(COMPILATORE) -c controller.c -o controller.o
+	$(COMPILATORE) -c genera_fascia.s -o genera_fascia.o
+clean:
+	rm -f $(ESEGUIBILE) core
+
